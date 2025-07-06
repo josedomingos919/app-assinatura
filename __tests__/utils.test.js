ccfe2f14ee67a594d80476ce60bae4e89a283af4
josedomingos919 <josedomingos919@gmail.com>
2025-07-06 01:43:32 +0100

    feat: test button click

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,10 +1,20 @@
 import { compararStringsLimpa } from "../src/app/home/util";
 import { isValidEmail } from "../src/app/singup/util";
 
+import { fireEvent, render } from "@testing-library/react-native";
+import { Button } from "../src/components";
+
 test("Deve falar que o email é inválido", () => {
   expect(isValidEmail("teste")).toBe(false);
 });
 
 test("Deve falar que os nomes são iguais", () => {
   expect(compararStringsLimpa("Antonio", "antónio")).toBe(true);
+});
+
+test("Deve clicar no botão", () => {
+  const mockFn = jest.fn();
+  const { getByText } = render(<Button title="Ola" onPress={mockFn} />);
+  fireEvent.press(getByText("Ola"));
+  expect(mockFn).toHaveBeenCalled();
 });
